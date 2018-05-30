0b2ec404f59e876d121a308cc8c1cc9793ae48d1
BirdyOz <greg.bird.oz@gmail.com>
2018-05-30 12:10:49 +1000

    3

--- dev/aiet/moodle-overrides.js
+++ dev/aiet/moodle-overrides.js
@@ -2,12 +2,16 @@
  * @Author: Greg Bird (@BirdyOz, greg.bird.oz@gmail.com)
  * @Date:   2018-05-30 11:19:44
  * @Last Modified by:   BirdyOz
- * @Last Modified time: 2018-05-30 11:54:00
+ * @Last Modified time: 2018-05-30 12:09:35
  */
 var time = "2018-05-30 11:23:03";
 console.log("@GB: Version = ", time);
 
 $(function() {
-    console.log("@GB: JQuery open");
+    $.each('.multianswer', function(index, val) {
+        console.log("@GB: val = ", val);
+         /* iterate through array or object */
+
+    });
 
 });
