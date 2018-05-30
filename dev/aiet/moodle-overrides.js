a8048b671ae2a9aa8d9114be9abea2ccbcf1856d
BirdyOz <greg.bird.oz@gmail.com>
2018-05-30 11:43:18 +1000

    first commit

--- dev/aiet/moodle-overrides.js
+++ dev/aiet/moodle-overrides.js
@@ -2,8 +2,12 @@
 * @Author: Greg Bird (@BirdyOz, greg.bird.oz@gmail.com)
 * @Date:   2018-05-30 11:19:44
 * @Last Modified by:   BirdyOz
-* @Last Modified time: 2018-05-30 11:23:26
+* @Last Modified time: 2018-05-30 11:42:51
 */
 
-var time = "2018-05-30 11:23:03";
-console.log("@GB: Version = ", time);
+
+$(function() {
+    var time = "2018-05-30 11:23:03";
+    console.log("@GB: Version = ", time);
+});
+
