b7ed8d66b2e7391b08b86211b7f751e5799bb8c3
BirdyOz <greg.bird.oz@gmail.com>
2018-05-30 11:24:48 +1000

    1

---
+++ dev/aiet/moodle-overrides.js
@@ -0,0 +1,9 @@
+/*
+* @Author: Greg Bird (@BirdyOz, greg.bird.oz@gmail.com)
+* @Date:   2018-05-30 11:19:44
+* @Last Modified by:   BirdyOz
+* @Last Modified time: 2018-05-30 11:23:26
+*/
+
+var time = "2018-05-30 11:23:03";
+console.log("@GB: Version = ", time);
