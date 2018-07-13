0db2ad2f503e5786748dde829a02b1d0f42263d0
BirdyOz <greg.bird.oz@gmail.com>
2018-07-13 16:09:26 +1000

    Moved fn alterclass into on ready statement

--- gotafe/js/theme-switcher.js
+++ gotafe/js/theme-switcher.js
@@ -2,29 +2,8 @@
  * @Author: Greg Bird (@BirdyOz, greg.bird.oz@gmail.com)
  * @Date:   2018-06-22 15:01:21
  * @Last Modified by:   BirdyOz
- * @Last Modified time: 2018-07-13 16:04:30
+ * @Last Modified time: 2018-07-13 16:08:39
  */
-
-// Swap out classes
-$.fn.alterClass = function(removals, additions) {
-    var self = this;
-    if (removals.indexOf('*') === -1) {
-        // Use native jQuery methods if there is no wildcard matching
-        self.removeClass(removals);
-        return !additions ? self : self.addClass(additions);
-    }
-    var patt = new RegExp('\\s' +
-        removals.replace(/\*/g, '[A-Za-z0-9-_]+').split(' ').join('\\s|\\s') +
-        '\\s', 'g');
-    self.each(function(i, it) {
-        var cn = ' ' + it.className + ' ';
-        while (patt.test(cn)) {
-            cn = cn.replace(patt, ' ');
-        }
-        it.className = $.trim(cn);
-    });
-    return !additions ? self : self.addClass(additions);
-};
 
 $(function() {
     // Switcher buttons - Trap click actions
@@ -37,5 +16,27 @@ $(function() {
         $('body').alterClass('gotafe-*', lastClass);
         event.preventDefault();
     });
-});
 
+
+    // Swap out classes
+    $.fn.alterClass = function(removals, additions) {
+        var self = this;
+        if (removals.indexOf('*') === -1) {
+            // Use native jQuery methods if there is no wildcard matching
+            self.removeClass(removals);
+            return !additions ? self : self.addClass(additions);
+        }
+        var patt = new RegExp('\\s' +
+            removals.replace(/\*/g, '[A-Za-z0-9-_]+').split(' ').join('\\s|\\s') +
+            '\\s', 'g');
+        self.each(function(i, it) {
+            var cn = ' ' + it.className + ' ';
+            while (patt.test(cn)) {
+                cn = cn.replace(patt, ' ');
+            }
+            it.className = $.trim(cn);
+        });
+        return !additions ? self : self.addClass(additions);
+    };
+
+});
