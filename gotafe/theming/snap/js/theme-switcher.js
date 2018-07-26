85fea01b0d43dcace164d226ec26c13e3b1a0f44
BirdyOz <greg.bird.oz@gmail.com>
2018-07-27 08:56:16 +1000

    Updated Styelswitcher

--- gotafe/theming/snap/js/theme-switcher.js
+++ gotafe/theming/snap/js/theme-switcher.js
@@ -2,41 +2,36 @@
  * @Author: Greg Bird (@BirdyOz, greg.bird.oz@gmail.com)
  * @Date:   2018-06-22 15:01:21
  * @Last Modified by:   BirdyOz
- * @Last Modified time: 2018-07-26 16:28:49
+ * @Last Modified time: 2018-07-27 08:55:49
  */
 
-
 jQuery(document).ready(function($) {
+
+    // Append stylesheet
+    $('head').append('<link rel="stylesheet" type="text/css" href="http://localhost:8888/gotafe/theming/snap/js/theme-switcher.css">');
+
+    // Set placeholder class.  To be swapped ou on mouxe click.
+    var curr_class = "gotafe-placeholder";
+    console.log("@GB: curr_class = ", curr_class);
+    // Define hook for body
+    var body = $('body');
+    // Add placeholder class
+    body.addClass(curr_class);
+
     // Switcher buttons - Trap click actions
     $('#switcher a').click(function(event) {
+        // Remove current class
+        body.removeClass(curr_class);
         var btn = $(this);
         lastClass = btn.attr('class').split(' ').pop();
+        // Get buttom class
         console.log("@GB: lastClass = ", lastClass);
+        curr_class = lastClass;
+        body.addClass(curr_class);
+        // Update heading
         var btntxt = btn.text();
+        console.log("@GB: btntxt = ", btntxt);
         $('#page-mast>h1>a').text(btntxt);
-        $('body').alterClass('gotafe-*', lastClass);
         event.preventDefault();
     });
 });
-
-(function($) {
-    $.fn.alterClass = function(removals, additions) {
-        var self = this;
-        if (removals.indexOf('*') === -1) {
-            // Use native jQuery methods if there is no wildcard matching
-            self.removeClass(removals);
-            return !additions ? self : self.addClass(additions);
-        }
-        var patt = new RegExp('\\s' +
-            removals.replace(/\*/g, '[A-Za-z0-9-_]+').split(' ').join('\\s|\\s') +
-            '\\s', 'g');
-        self.each(function(i, it) {
-            var cn = ' ' + it.className + ' ';
-            while (patt.test(cn)) {
-                cn = cn.replace(patt, ' ');
-            }
-            it.className = $.trim(cn);
-        });
-        return !additions ? self : self.addClass(additions);
-    };
-})(jQuery);
