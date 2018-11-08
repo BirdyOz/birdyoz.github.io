6bb31e35b522d622727b6324dc1631e4cc9f3a87
Greg Bird <greg.bird.oz@gmail.com>
2018-11-09 09:23:02 +1100

    Updates to inject login settings to include placeholder text

--- gotafe/theming/snap/js/inject-login-form.js
+++ gotafe/theming/snap/js/inject-login-form.js
@@ -1,56 +1,9 @@
-/*
- * @Author: Greg Bird (@BirdyOz, greg.bird.oz@gmail.com)
- * @Date:   2018-06-22 15:01:21
- * @Last Modified by:   BirdyOz
- * @Last Modified time: 2018-08-02 08:57:42
- */
-
-jQuery(document).ready(function($) {
-
-    // Append stylesheet
-    $('head').append('<link rel="stylesheet" type="text/css" href="https://birdyoz.github.io/gotafe/theming/snap/css/theme-switcher.css">');
-    // $('head').append('<link rel="stylesheet" type="text/css" href="http://localhost:8888/gotafe/theming/snap/css/theme-switcher.css">');
-
-    // Set placeholder class.  To be swapped ou on mouxe click.
-    var curr_class = "gotafe-placeholder";
-    console.log("@GB: curr_class = ", curr_class);
-    // Define hook for body
-    var body = $('body');
-    // Add placeholder class
-    body.addClass(curr_class);
-
-    // Switcher buttons - Trap click actions
-    $('#switcher a').click(function(event) {
-        // Remove current class
-        body.removeClass(curr_class);
-        var btn = $(this);
-        lastClass = btn.attr('class').split(' ').pop();
-        // Get buttom class
-        console.log("@GB: lastClass = ", lastClass);
-        curr_class = lastClass;
-        body.addClass(curr_class);
-        // Update heading
-        var btntxt = btn.text();
-        console.log("@GB: btntxt = ", btntxt);
-        $('#page-mast>h1>a').text(btntxt);
-        event.preventDefault();
-    });
-
-    // Toggle heights
-    $('a.toggle-banner-height').click(function(event) {
-        body.toggleClass('gotafe-narrow-banner');
-        event.preventDefault();
-    });
-
-    // Toggle contrast
-    $('a.toggle-high-contrast').click(function(event) {
-        body.toggleClass('gotafe-high-contrast');
-        event.preventDefault();
-    });
-
-    // Toggle images
-    $('a.toggle-banner-image').click(function(event) {
-        body.toggleClass(curr_class);
-        event.preventDefault();
-    });
+<!-- JS injection   Inject login form into the pre-login screen -->
+<script type="text/javascript">
+$(document).ready(function() {
+    var snip = '<div class="card-text content" id="gotafe-frontpage-login"> <form class="loginform" id="login" method="post" action="/login/index.php"> <div class="form-group"> <label class="hidden" for="login_username">Username</label> <input name="username" id="login_username" class="form-control" value="" type="text" placeholder="Username"> </div> <div class="form-group"> <label class="hidden" for="login_password">Password</label> <input name="password" id="login_password" class="form-control" value="" type="password" placeholder="Password"> </div> <div class="form-check"> <label class="form-check-label"> <input name="rememberusername" id="rememberusername" class="form-check-input" value="1" checked="checked" type="checkbox"> Remember username</label> </div> <div class="form-group gotafe-frontpage-submit"> <div class="row"> <div class="col-sm-9"> <input class="btn btn-primary btn-block" value="Log in" type="submit"> </div> <div class="col-sm-3 text-right"> <p class="text-right"><small><a href="/forgot_password.php">Forgot password?</a></small></p> </div> </div> </div> </form> </div>';
+    // var snip = 'Inject login form here';
+    $('#page-mast .snap-login-button').before(snip);
+    $('#page-mast .snap-login-button').hide();
 });
+</script>
