a4bb7d8b64b10d08302d8c8d3c9403216b58158e
Greg Bird <greg.bird.oz@gmail.com>
2019-04-03 13:42:47 +1100

    Pixabay attribution helper.  First commit

---
+++ helpers/pixabay_attribution_helper.js
@@ -0,0 +1,102 @@
+/*
+ * @Author: Greg Bird (@BirdyOz, greg.bird.oz@gmail.com)
+ * @Date:   2018-05-10 10:37:58
+ * @Last Modified by:   Greg Bird
+ * @Last Modified time: 2019-04-03 13:23:46
+ */
+
+$(function() {
+
+    var url_string = window.location.href;
+    if (url_string.indexOf("?") > 0) {
+        var url = new URL(url_string);
+        var statement = url.searchParams.get("statement");
+        console.log("@GB: statement = ", statement);
+
+        if (statement.length > 0) {
+
+            var img = $.parseHTML(url.searchParams.get("img"));
+            var alt = $(img).attr("alt");
+            var src = $(img).attr("src");
+            console.log("@GB: src = ", src);
+            console.log("@GB: alt = ", alt);
+
+
+            $('img').each(function(index, el) {
+                el.src = src;
+                $(this).attr('alt', alt);
+                $(this).attr('title', alt);
+            });
+
+            $('small').each(function(index, el) {
+                $(this).html(statement);
+            });
+
+            $('#embedder button').click(function(event) {
+                /* Act on the event */
+
+                var btn = $(this);
+                var closest = btn.prev('.unsplash-copy');
+                var id = "." + btn.attr('id');
+                var paste = $(id).html();
+                console.log("@GB: paste = ", paste);
+
+                copyTextToClipboard(paste);
+
+
+                btn.toggleClass('btn-outline-primary btn-success');
+                btn.html('<i class="fa fa-check" aria-hidden="true"></i> Done! Embed code copied to clipboard');
+
+                window.setTimeout(function() {
+                    btn.html('<i class="fa fa-clipboard" aria-hidden="true"></i> Copy embed code');
+                    // btn.removeClass('btn-danger');
+                    btn.toggleClass('btn-outline-primary btn-success');
+                }, 3000);
+
+                // Cancel the default action
+                event.preventDefault();
+            });
+        } else {
+            $('img').each(function(index, el) {
+                el.src = "https://dummyimage.com//b094b0/e3b1e3&text=Seeing+this?++instructions+above";
+            });
+        }
+    } else {
+        console.log("@GB: No parameters");
+        $('img').each(function(index, el) {
+            el.src = "https://dummyimage.com//b094b0/e3b1e3&text=First+time?++instructions+above";
+        });
+
+    }
+
+    function fallbackCopyTextToClipboard(text) {
+        var textArea = document.createElement("textarea");
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+
+        } catch (err) {
+
+        }
+
+        document.body.removeChild(textArea);
+    }
+
+    function copyTextToClipboard(text) {
+        if (!navigator.clipboard) {
+            fallbackCopyTextToClipboard(text);
+            return;
+        }
+        navigator.clipboard.writeText(text).then(function() {
+
+        }, function(err) {
+
+        });
+    }
+
+});
