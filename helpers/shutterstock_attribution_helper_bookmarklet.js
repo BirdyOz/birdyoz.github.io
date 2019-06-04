728f81d40b3af666a5cbf781849a1351b765e5c3
Greg Bird <greg.bird.oz@gmail.com>
2019-06-04 18:52:56 +1000

    First commit

---
+++ helpers/shutterstock_attribution_helper_bookmarklet.js
@@ -0,0 +1,10 @@
+var addr = document.location.href.split("?")[0];
+console.log("@GB: addr = ", addr);
+var el = document.getElementsByClassName("product-contrib")[0].getElementsByTagName("h3")[0];
+console.log("@GB: el = ", el);
+var user = encodeURIComponent(el.innerHTML);
+console.log("@GB: user = ", user);
+var url = 'https://birdyoz.github.io/helpers/shutterstock_attribution_helper.html?addr=' + addr + '&user=' + user;
+console.log("@GB: url = ", url);
+window.open(url, '_blank')
+
