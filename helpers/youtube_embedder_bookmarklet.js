d6ad7eb6f86d19926de2d64c7fbbc51edb3ac370
Greg Bird <greg.bird.oz@gmail.com>
2019-06-04 13:11:04 +1000

    Updated Jquery version and integrity check.

--- helpers/youtube_embedder_bookmarklet.js
+++ helpers/youtube_embedder_bookmarklet.js
@@ -1,9 +1,7 @@
 var url_string = window.location.href;
-    if (url_string.indexOf("?") > 0) {
-        var url = new URL(url_string);
-        var yt_video_id = url.searchParams.get("v");
-        var url = 'https://birdyoz.github.io/helpers/pixabay_attribution_helper.html?src='+src+'&statement='+statement+'&alt='+alt;
-window.open(url,'_blank');
-    }
-
-
+if (url_string.indexOf("?") > 0) {
+    var url = new URL(url_string);
+    var ytv = url.searchParams.get("v");
+    var url = 'https://birdyoz.github.io/helpers/youtube_embedder.html?yt_video_id=' + ytv;
+    window.open(url, '_blank');
+}
