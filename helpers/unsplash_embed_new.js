319594046c2d4178e92e8d12f230a0c1e90d4075
Greg Bird <greg.bird.oz@gmail.com>
2019-06-21 14:47:58 +1000

    Delivery release of Unsplash Embedder 2

--- helpers/unsplash_embed_new.js
+++ helpers/unsplash_embed_new.js
@@ -2,28 +2,29 @@
  * @Author: Greg Bird (@BirdyOz, greg.bird.oz@gmail.com)
  * @Date:   2018-05-10 10:37:58
  * @Last Modified by:   Greg Bird
- * @Last Modified time: 2019-06-21 09:47:13
+ * @Last Modified time: 2019-06-21 14:43:33
  */
 
 $(function() {
+    // Detect IE
+    var isIE = detectIE();
+    // Hide IE alert for non-IE browsers
+    if (isIE === true) {
+        $(".alert-ie").toggleClass('collapse show');
+    };
 
     var url_string = window.location.href;
     if (url_string.indexOf("?") > 0) {
-        console.log("@GB: Has parameters");
-        // Extract URL variable
+        // hide "first time" alert
+
+
+        // Extract URL variables
         var url = new URL(url_string);
         var addr = url.searchParams.get("addr");
-        console.log("@GB: addr = ", addr);
         var title = url.searchParams.get("title");
-        console.log("@GB: title = ", title);
-        // Establish image sizes
-        var img_lge = decodeURIComponent(url.searchParams.get("img"));
-        img_lge = img_lge.replace("http://", "https://");
-        console.log("@GB: img_lge = ", img_lge);
-        var img_med = img_lge.replace("w=1080", "w=720");
-        console.log("@GB: img_med = ", img_med);
-        var img_sml = img_lge.replace("w=1080", "w=360");
-        console.log("@GB: img_sml = ", img_sml);
+
+
+        // Pull apart Unsplash title to form an array
         var re = /by (.*?) \((.*?)\)/gi;
         var TitleArray = re.exec(title);
         console.log("@GB: TitleArray = ", TitleArray);
@@ -44,7 +45,29 @@ $(function() {
             var yyyy = today.getFullYear();
             today = dd + '/' + mm + '/' + yyyy;
 
+            // Create attribution statement
             var dom = '<a href="' + addr + '" target="_blank">Photo</a> by <a href="https://unsplash.com/' + user + '" target="_blank">' + user + '</a> on <a href="https://unsplash.com" target="_blank">Unsplash</a>, accessed ' + today;
+
+            // Establish Unsplash image path
+            var img_lge = decodeURIComponent(url.searchParams.get("img"));
+
+            if (img_lge !== 'null') {
+                console.log("@GB: img_lge = ", img_lge);
+                // convert to https://
+                img_lge = img_lge.replace("http://", "https://");
+                // Create medium and small variants
+                var img_med = img_lge.replace("w=1080", "w=720");
+                var img_sml = img_lge.replace("w=1080", "w=360");
+            } else {
+                img_sml = img_url;
+                img_med = img_url;
+                img_lge = img_url;
+                console.log("@GB: img_sml = ", img_sml);
+                $(".alert-new").toggleClass('collapse show');
+                // hide download image buttons
+                $('a.download').hide();
+            }
+
 
             // Inject images of appropriate sizes
             $('img.img-sml').each(function(index, el) {
@@ -102,6 +125,7 @@ $(function() {
                 if (title === "img-sml") { dl_img = img_sml };
                 if (title === "img-med") { dl_img = img_med };
 
+                // Parse image to Canvas download
                 downloader(img, dl_img);
 
                 btn.toggleClass('btn-outline-primary btn-success');
@@ -111,12 +135,14 @@ $(function() {
                 event.preventDefault();
             });
         } else {
+
             $('img').each(function(index, el) {
                 el.src = "https://dummyimage.com//b094b0/e3b1e3&text=Seeing+this?++instructions+above";
             });
         }
     } else {
-        console.log("@GB: No parameters");
+        // If no URL parameters, inject placeholder images
+        $(".alert-ft").toggleClass('collapse show');
         $('img').each(function(index, el) {
             el.src = "https://dummyimage.com//b094b0/e3b1e3&text=First+time?++instructions+above";
         });
@@ -183,7 +209,35 @@ $(function() {
 
     }
 
+    /**
+     * detect IE
+     * returns version of IE or false, if browser is not Internet Explorer
+     */
+    function detectIE() {
+        var ua = window.navigator.userAgent;
 
+        var msie = ua.indexOf('MSIE ');
+        if (msie > 0) {
+            // IE 10 or older => return version number
+            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+        }
+
+        var trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+            // IE 11 => return version number
+            var rv = ua.indexOf('rv:');
+            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
+
+        var edge = ua.indexOf('Edge/');
+        if (edge > 0) {
+            // Edge (IE 12+) => return version number
+            return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+        }
+
+        // other browser
+        return false;
+    }
 
 
 });
