97171e6b3b3e7c17ce0f3e38a502f7bcffcb505a
BirdyOz <greg.bird.oz@gmail.com>
2021-08-02 11:07:35 +1000

    Reverted API key

--- helpers/youtube_embed.js
+++ helpers/youtube_embed.js
@@ -2,7 +2,7 @@
  * @Author: Greg Bird (@BirdyOz, greg.bird.oz@gmail.com)
  * @Date:   2018-05-10 10:37:58
  * @Last Modified by:   BirdyOz
- * @Last Modified time: 2021-08-02 11:03:55
+ * @Last Modified time: 2021-08-02 11:07:05
  */
 
 $(function() {
@@ -18,8 +18,8 @@ $(function() {
         // Make YouTube API call
         if (yt_video_id.length > 0) {
             // Define global vars
-            // var yt_api_key = "AIzaSyBlBpATO1tgHN3qrPe0ZT9haE1nTBlQaU4",
-            var yt_api_key = "AIzaSyDFPyUHKht-uI7r4sUggbO3bvq7woHAjd0",
+            var yt_api_key = "AIzaSyBlBpATO1tgHN3qrPe0ZT9haE1nTBlQaU4",
+            // var yt_api_key = "AIzaSyDFPyUHKht-uI7r4sUggbO3bvq7woHAjd0",
                 // yt_video_id = "Rtmj-oE6wPU",
                 yt_snippet_endpoint = "https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatus%2Cplayer&id=" + yt_video_id + "&key=" + yt_api_key;
             console.log("@GB: yt_snippet_endpoint = ", yt_snippet_endpoint);
