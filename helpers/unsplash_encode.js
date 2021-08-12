6a776d5066d336ef499e698bdc4ec9fc812798f9
BirdyOz <greg.bird.oz@gmail.com>
2021-08-12 16:32:56 +1000

    Full redevelopment - Uses Unsplash API - resizable images

--- helpers/unsplash_encode.js
+++ helpers/unsplash_encode.js
@@ -1,112 +1,101 @@
 /*
  * @Author: Greg Bird (@BirdyOz, greg.bird.oz@gmail.com)
  * @Date:   2018-05-10 10:37:58
- * @Last Modified by:   Greg Bird
- * @Last Modified time: 2019-06-19 15:31:26
+ * @Last Modified by:   BirdyOz
+ * @Last Modified time: 2021-08-12 16:27:03
  */
 
 $(function() {
 
+    // Get current date string
+    var today = todaysDate();
+    var src = "";
+    var alt = "";
+    var title = "";
+    var user = "";
+    var link = "";
+    var width = "col-5";
+    var percent = "42%";
+    console.log("@GB: today = ", today);
+    var id = "";
     var url_string = window.location.href;
-    var id = "Be8TdJZPaBE";
-    var uri = "https://api.unsplash.com/photos/" + id + "?client_id=336b527b2e18d045045820b78062b95c825376311326b2a08f9b93eef7efc07b";
-    $.getJSON( uri, function( result ) {
-        console.log("@GB: result = ", result);
-            full = result.urls.full;
-            console.log("@GB: full = ", full);
-            regular = result.urls.regular;
-            console.log("@GB: regular = ", regular);
-            small = result.urls.small;
-            console.log("@GB: small = ", small);
-            user = result.user.username;
-            console.log("@GB: user = ", user);
-            link = result.user.links.html;
-            console.log("@GB: link = ", link);
-
-            desc = result.description;
-            console.log("@GB: desc = ", desc);
-            alt_desc = result.alt_description;
-            console.log("@GB: alt_desc = ", alt_desc);
-
-
-        });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     if (url_string.indexOf("?") > 0) {
         console.log("@GB: Has parameters");
-
         var url = new URL(url_string);
-        var addr = url.searchParams.get("addr");
-        var title = url.searchParams.get("title");
-        var re = /by (.*?) \((.*?)\)/gi;
-        var TitleArray = re.exec(title);
-        console.log("@GB: TitleArray = ", TitleArray);
-        if (TitleArray !== null) {
-
-            var name = TitleArray[1];
-            var user = TitleArray[2];
-            var image = addr.substring(addr.lastIndexOf("/") + 1, addr.length);
-            var image_url = "https://source.unsplash.com/" + image;
-            var dom = '<a href="' + addr + '" target="_blank">Photo</a> by <a href="https://unsplash.com/' + user + '" target="_blank">' + user + '</a> on <a href="https://unsplash.com" target="_blank">Unsplash</a>';
-
-
-            $('img').each(function(index, el) {
-                el.src = image_url;
-                $(this).attr('alt', title);
-                $(this).attr('title', title);
-            });
-
-            $('small').each(function(index, el) {
-                $(this).html(dom);
-            });
-
-            $('#embedder button').click(function(event) {
-                /* Act on the event */
-
-                var btn = $(this);
-                var closest = btn.prev('.unsplash-copy');
-                var id = "." + btn.attr('id');
-                var paste = $(id).html();
-                console.log("@GB: paste = ", paste);
-
-                copyTextToClipboard(paste);
-
-
-                btn.toggleClass('btn-outline-primary btn-success');
-                btn.html('<i class="fa fa-check" aria-hidden="true"></i> Done! Embed code copied to clipboard');
-
-                window.setTimeout(function() {
-                    btn.html('<i class="fa fa-clipboard" aria-hidden="true"></i> Copy embed code');
-                    // btn.removeClass('btn-danger');
-                    btn.toggleClass('btn-outline-primary btn-success');
-                }, 3000);
-
-                // Cancel the default action
-                event.preventDefault();
-            });
-        } else {
-            $('img').each(function(index, el) {
-                el.src = "https://dummyimage.com//b094b0/e3b1e3&text=Seeing+this?++instructions+above";
-            });
-        }
+        id = url.searchParams.get("id");
     } else {
         console.log("@GB: No parameters");
-        $('img').each(function(index, el) {
-            el.src = "https://dummyimage.com//b094b0/e3b1e3&text=First+time?++instructions+above";
+        id = "TXxiFuQLBKQ";
+    }
+
+    var uri = "https://api.unsplash.com/photos/" + id + "?client_id=336b527b2e18d045045820b78062b95c825376311326b2a08f9b93eef7efc07b";
+    $.getJSON(uri, function(result) {
+        console.log("@GB: result = ", result);
+        src = result.urls.full;
+        console.log("@GB: src = ", src);
+        user = result.user.username;
+        console.log("@GB: user = ", user);
+        link = result.user.links.html;
+        console.log("@GB: link = ", link);
+
+        title = result.description;
+        console.log("@GB: title = ", title);
+        alt = result.alt_description;
+        console.log("@GB: alt = ", alt);
+
+
+        $('.unsplash-copy>figure').each(function(index) {
+            snippet = unsplashSnippet(index);
+            $(this).html(snippet);
         });
 
+        $('#source-open').change(function() {
+            selected_val = $("input[name='options']:checked").attr('value');
+            console.log("@GB: selected_val = ", selected_val);
+            if (selected_val == "Shown") {
+                $('.source-btn').addClass('collapse');
+                $('.source').addClass('show');
+            } else {
+                $('.source-btn').removeClass('collapse');
+                $('.source').removeClass('show');
+            }
+        });
+
+        $('#resizer').change(function() {
+            selected_id = $("input[name='options']:checked").attr('id');
+            selected_val = $("input[name='options']:checked").attr('value');
+            console.log("@GB: selected_text = ", selected_val);
+            console.log("@GB: selected_id = ", selected_id);
+            $('.unsplash-floated>figure').removeClass(width);
+            $('.unsplash-floated>figure').addClass(selected_id);
+            $('.percent>strong').text(selected_val);
+            width = selected_id;
+        });
+    });
+
+    function unsplashSnippet(i) {
+        var snippet = `
+        <img src="${src}" class="img-responsive img-fluid img-sml" alt="${alt}" title="${title}">
+        <figcaption class="figure-caption text-muted small">
+            <small>
+                <!-- Start of Show/Hide interface, ID = ${id}-${i} -->
+                <a class="source-btn" data-toggle="collapse" href="#show-${id}-${i}" role="button" aria-expanded="false" aria-controls="show-${id}-${i}">Source:</a>
+                <div class="source collapse m-0 p-0" id="show-${id}-${i}"> <a href="https://unsplash.com/photos/${id}" target="_blank">Photo</a> by <a href="${link}" target="_blank">${user}</a> on <a href="https://unsplash.com" target="_blank">Unsplash</a>
+                    <br><a href="https://unsplash.com/license" target="_blank">Free to use</a>. Added ${today} </div>
+                <!-- End of Show/Hide interface, ID = ${id}-${i} -->
+            </small>
+        </figcaption>
+    `;
+        return snippet;
+    }
+
+    function todaysDate() {
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+        today = dd + '/' + mm + '/' + yyyy;
+        return today;
     }
 
     function fallbackCopyTextToClipboard(text) {
