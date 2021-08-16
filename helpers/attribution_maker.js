2645ea4970dec09384099ce10d51b1cbc9048bff
BirdyOz <greg.bird.oz@gmail.com>
2021-08-16 15:50:01 +1000

    Added support for Pixabay

--- helpers/attribution_maker.js
+++ helpers/attribution_maker.js
@@ -2,85 +2,111 @@
  * @Author: Greg Bird (@BirdyOz, greg.bird.oz@gmail.com)
  * @Date:   2018-05-10 10:37:58
  * @Last Modified by:   BirdyOz
- * @Last Modified time: 2021-08-13 13:17:12
+ * @Last Modified time: 2021-08-16 15:41:36
  */
 
 $(function() {
 
     // Get current date string
-    var today = todaysDate();
-    var src = "";
-    var alt = "";
-    var title = "";
-    var user = "";
-    var link = "";
-    var id = "";
-    var startCollapsed = true;
-    var maxpx = 1440;
-    var width = "col-5";
-    var dimensions = [{"id": "col-1", "percent": "8%", "px": 120 },
-                      {"id": "col-2", "percent": "17%", "px": 240 },
-                      {"id": "col-3", "percent": "25%", "px": 360 },
-                      {"id": "col-4", "percent": "33%", "px": 480 },
-                      {"id": "col-5", "percent": "42%", "px": 600 },
-                      {"id": "col-6", "percent": "50%", "px": 720 },
-                      {"id": "col-7", "percent": "58%", "px": 840 },
-                      {"id": "col-8", "percent": "67%", "px": 960 },
-                      {"id": "col-9", "percent": "75%", "px": 1080 },
-                      {"id": "col-10", "percent": "83%", "px": 1200 },
-                      {"id": "col-11", "percent": "92%", "px": 1320 },
-                      {"id": "col-12", "percent": "100%", "px": 1440 }];
-    var url_string = window.location.href;
+    let today = todaysDate();
+    let site = "";
+    let site_url = "";
+    let id = ""; // Image ID
+    let img_name = "Photo"; // Image name.   Defualt to "Photo"
+    let img_orig = ""; // Link to original image
+    let img_src = ""; // src of image to be displayed in page
+    let download_sml = ""; // Small image 960px wide
+    let download_lge = ""; // Large image 1920px wide
+    let alt = "";
+    let user = "";
+    let user_url = "";
+    let licence = "";
+    let licence_url = "";
+    let title = null; //If a title is set, this will be used
+    let startCollapsed = true;
+    let width = "col-5"; // Default width for floated images
+    // Get URL parameters
+    url_string = window.location.href;
     if (url_string.indexOf("?") > 0) {
-        var url = new URL(url_string);
-        console.log("@GB: url = ", url);
-        var addr = url.searchParams.get("addr");
-        console.log("@GB: addr = ", addr);
-        var n = addr.lastIndexOf('/');
-        var id = addr.substring(n + 1);
+        url = new URL(url_string);
+        img_orig = url.searchParams.get("addr");
+        // Detect site
+        if (img_orig.includes('unsplash')) {
+            site = "Unsplash";
+        }
+        if (img_orig.includes('pixabay')) {
+            site = "Pixabay";
+            $('.pixabay-warning').show();
+        }
 
     } else {
         console.log("@GB: No parameters");
+        $('.firsttime-warning').show();
+        $('#collapseExample').show();
     }
 
-    var uri = "https://api.unsplash.com/photos/" + id + "?client_id=336b527b2e18d045045820b78062b95c825376311326b2a08f9b93eef7efc07b";
-    $.getJSON(uri, function(result) {
-        console.log("@GB: result = ", result);
-        src = result.urls.regular;
-        console.log("@GB: src = ", src);
-        user = result.user.username;
-        console.log("@GB: user = ", user);
-        link = result.user.links.html;
-        console.log("@GB: link = ", link);
+    // If I am Unsplash
+    if (site == "Unsplash") {
+        n = img_orig.lastIndexOf('/');
+        id = img_orig.substring(n + 1);
+        site_url = "https://unsplash.com";
+        licence = "Free to use";
+        licence_url = "https://unsplash.com/licence";
 
-        title = result.description;
-        console.log("@GB: title = ", title);
-        alt = result.alt_description;
-        console.log("@GB: alt = ", alt);
+        // API call
+        uri = "https://api.unsplash.com/photos/" + id + "?client_id=336b527b2e18d045045820b78062b95c825376311326b2a08f9b93eef7efc07b";
 
-        buildHTML();
+        $.getJSON(uri, function(result) {
+            console.log("@GB: result = ", result);
+            img_src = result.urls.regular;
+            console.log("@GB: img_src = ", img_src);
+            user = result.user.username;
+            user_url = result.user.links.html;
+            title = result.description;
+            alt = result.alt_description;
+            download_lge = img_src.replace("&w=1080", "&w=1440");
+            img_src=download_lge;
+            download_sml = img_src.replace("&w=1080", "&w=720");
+            buildHTML();
+        });
+    }
 
-    });
+    // If I am Pixabay
+    if (site == "Pixabay") {
+        re = /[0-9]+/gi;
+        id = re.exec(img_orig)[0];
+        console.log("@GB: Pixabay id = ", id);
+        site_url = "https://pixabay.com/";
+        licence = "Free to use";
+        licence_url = "https://pixabay.com/service/license/";
+        api_key = "11445-7c3e3173d6f9a6047e64583ca";
+        uri = "https://pixabay.com/api/?key=" + api_key + "&id=" + id;
+        maxpx = 1280;
 
-    $('#source-open').change(function() {
-        selected_val = $("input[name='options']:checked").attr('value');
-        console.log("@GB: selected_val = ", selected_val);
-        if (selected_val == "Shown") {
-            $('.source-btn').addClass('collapse');
-            $('.source').addClass('show');
-        } else {
-            $('.source-btn').removeClass('collapse');
-            $('.source').removeClass('show');
-        }
-    });
+        $.getJSON(uri, function() {})
+            .done(function(data) {
+                console.log("second success");
+                console.log("@GB: data = ", data.hits[0]);
+                img_src = data.hits[0].largeImageURL;
+                console.log("@GB: img_src = ", img_src);
+                user = data.hits[0].user;
+                user_url = data.hits[0].userImageURL;
+                alt = data.hits[0].tags;
+                img_name = "Image";
+                download_sml = data.hits[0].webformatURL; // Small image 640px wide
+                download_lge = img_src; // Large image 1280px wide
+                buildHTML();
+            });
+    }
+
 
     $('#resizer').change(function() {
         selected_id = $("input[name='options']:checked").attr('id');
         selected_val = $("input[name='options']:checked").attr('value');
         console.log("@GB: selected_text = ", selected_val);
         console.log("@GB: selected_id = ", selected_id);
-        $('.unsplash-floated>figure').removeClass(width);
-        $('.unsplash-floated>figure').addClass(selected_id);
+        $('.maker-floated>figure').removeClass(width);
+        $('.maker-floated>figure').addClass(selected_id);
         $('.percent').text(selected_val);
         width = selected_id;
         console.log("@GB: width = ", width);
@@ -104,7 +130,7 @@ $(function() {
         // Cancel the default action
         event.preventDefault();
         var btn = $(this);
-        var closest = btn.prev('.unsplash-copy');
+        var closest = btn.prev('.maker-copy');
         var id = "." + btn.attr('id');
         var paste = $(id).html();
         console.log("@GB: paste = ", paste);
@@ -127,21 +153,14 @@ $(function() {
 
     $('a.download').click(function(event) {
         /* Act on the event */
-        var btn = $(this);
-        var title = btn.attr("title");
-        var px = maxpx;
+        btn = $(this);
+        title = btn.attr("title");
+        src = download_lge;
         if (title == "img-sml") {
-            var result = dimensions.filter(function(e){return e.id == width;});
-            console.log(result);
-            px = result[0].px;
-            console.log("@GB: px = ", px);
+            src = download_sml;
         }
-        var regex = /&w\=[0-9]+/i;
-        src = src.replace(regex,"&w="+px);
-        console.log("@GB: src = ", src);
-
-        // Parse image to Canvas download
-        downloader(id,src);
+        console.log("@GB: download_url = ", src);
+        downloader(id, src);
 
         btn.toggleClass('btn-outline-primary btn-success');
         btn.html('<i class="fa fa-check" aria-hidden="true"></i> Done! Image downloaded');
@@ -152,15 +171,15 @@ $(function() {
 
     function unsplashSnippet(i) {
         var snippet = `
-        <img src="${src}" class="img-responsive img-fluid img-sml" alt="${alt}" title="${title}">
+        <img src="${img_src}" class="img-responsive img-fluid img-sml" alt="${alt}"${title!==null ? ` title="${title}"` : ''}>
         <figcaption class="figure-caption text-muted small">
             <small>
                 ${startCollapsed ? `
                 <!-- Start of Show/Hide interface, ID = ${id}-${i} -->
                 <a class="source-btn" data-toggle="collapse" href="#show-${id}-${i}" role="button" aria-expanded="false" aria-controls="show-${id}-${i}">&#9660; Show attribution</a>
                 <div class="source collapse m-0 p-0" id="show-${id}-${i}">` : ''}
-                <a href="https://unsplash.com/photos/${id}" target="_blank">Photo</a> by <a href="${link}" target="_blank">${user}</a> on <a href="https://unsplash.com" target="_blank">Unsplash</a>
-                    <br><a href="https://unsplash.com/license" target="_blank">Free to use</a>. Added ${today} ${startCollapsed ? `</div>
+                <a href="${img_orig}" target="_blank">${img_name}</a> by <a href="${user_url}" target="_blank">${user}</a> on <a href="${site_url}" target="_blank">${site}</a>
+                    <br><a href="${licence_url}" target="_blank">${licence}</a>. Added ${today} ${startCollapsed ? `</div>
                 <!-- End of Show/Hide interface, ID = ${id}-${i} -->` : ''}
             </small>
         </figcaption>
@@ -169,7 +188,7 @@ $(function() {
     }
 
     function buildHTML() {
-        $('.unsplash-copy figure').each(function(index) {
+        $('.maker-copy figure').each(function(index) {
             snippet = unsplashSnippet(index);
             $(this).html(snippet);
         });
@@ -233,7 +252,7 @@ $(function() {
             link.style = 'position: fixed; left -10000px;';
             link.href = blob;
 
-            link.download = "Unsplash-" + name + "-" + canvas.width + "x" + canvas.height + ".jpg";
+            link.download = site + "-" + name + "-" + canvas.width + "x" + canvas.height + ".jpg";
             console.log("@GB: link.download = ", link.download);
 
 
